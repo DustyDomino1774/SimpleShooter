a283893cad1b640ac2158f12cfd7b4a790faa3f0
Dominic London <londonrdominic@gmail.com>
2023-11-15 01:37:32 -0800

    Controller Aiming

--- Source/SimpleShooter/ShooterCharacter.h
+++ Source/SimpleShooter/ShooterCharacter.h
@@ -29,5 +29,10 @@ public:
 private:
 	void MoveForward(float AxisValue);
 	void MoveRight(float AxisValue);
+	void LookUpRate(float AxisVlaue);
+	void LookRightRate(float AxisVlaue);
+
+	UPROPERTY(EditAnywhere)
+	float RotationRate = 10;
 
 };
